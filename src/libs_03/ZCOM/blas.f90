bc3a7b5b2dbfc21117257ef2f35aa0f4d5163070
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2025-01-22 18:09:00 +0100

    Added routines to dynamically get/set the number of BLAS threads

--- src/libs_03/ZCOM/blas.f90
+++ src/libs_03/ZCOM/blas.f90
@@ -2,7 +2,8 @@ module BLAS
   implicit none
   private
 
-  public general_rank_update
+  public general_rank_update, &
+       blas_set_num_threads, blas_get_num_threads
 
   interface general_rank_update
      module procedure sgeneral_rank_update
@@ -10,6 +11,23 @@ module BLAS
      module procedure cgeneral_rank_update
      module procedure zgeneral_rank_update
   end interface general_rank_update
+
+  interface
+#ifndef BLA_VENDOR
+#error "You must set BLA_VENDOR as a compile definition"
+#elif BLA_VENDOR == OpenBLAS
+     subroutine blas_set_num_threads(n) bind(C, name='openblas_set_num_threads')
+       use ISO_C_BINDING
+       integer(C_INT), intent(in), value :: n
+     end subroutine blas_set_num_threads
+
+     integer(C_INT) function blas_get_num_threads() bind(C, name='openblas_get_num_threads')
+       use ISO_C_BINDING
+     end function blas_get_num_threads
+#else
+#error "Implementation of blas_{s,g}et_num_threads missing for chosen BLA_VENDOR"
+#endif
+  end interface
 contains
   ! * BLAS 2
   ! ** General rank update
